03557714eca25493a7731da177306d46e237e5a0
Jens John <dev@2ion.de>
2014-04-26 00:08:03 +0900

    re-write for Lua 5.3 & utf8 support

--- column.lua
+++ column.lua
@@ -1,1 +1,21 @@
-#!/usr/bin/env lua
+#!./lua
+
+assert(_VERSION == "Lua 5.3", "This program requires Lua 5.3")
+
+local function columnate(s, csep, rsep)
+  local r = { { {} } }
+  for _,c in utf8.codes(s) do
+    local c = utf8.char(c)
+    local cr = r[#r]
+    local cc = cr[#cr]
+    if c == csep then
+      table.insert(cr, {})
+    elseif c == rsep then
+      table.insert(r, {})
+    else
+      table.insert(cc, c)
+    end
+  end
+end
+
+columnate("Hello World", ' ')
